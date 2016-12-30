32ef742c2a0c7fd6204c1fd2406f40e59681143d
belshick <belshick.megasoncse@ksrce.ac.in>
2016-12-30 10:21:44 +0530

    Add files via upload

---
+++ posrneg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int n;
+    printf("\n enter the no:");
+    scanf("%d",&n);
+    if(n>=0)
+    {
+
+        printf("postive");
+    }
+    else
+    {
+        printf("negative");
+    }
+
+}
