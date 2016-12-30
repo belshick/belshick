21c27f5c21b0502c6852c65be682bb8d655f825a
belshick <belshick.megasoncse@ksrce.ac.in>
2016-12-30 10:27:59 +0530

    Add files via upload

---
+++ evenrodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int a;
+    printf("\n enter the no");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+
+        printf("even no");
+    }
+    else
+        {
+        printf("odd no");
+    }
+}
