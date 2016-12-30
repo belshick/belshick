8ff8f949e5170f0e3ca581a7d38b2e514e88e62a
belshick <belshick.megasoncse@ksrce.ac.in>
2016-12-30 10:39:21 +0530

    Add files via upload

--- vi.c
+++ vi.c
@@ -5,7 +5,7 @@ int main()
     char a;
     printf("\n enter the character");
     scanf("%c",&a);
-    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
     {
 
         printf("\n vowel");
