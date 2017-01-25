7ecc9d4cb2b699a594a31932f6eab5e7e4324e2c
belshick <belshick.megasoncse@ksrce.ac.in>
2017-01-25 12:02:49 +0530

    Add files via upload

---
+++ ADD2 IN.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int a,b,i;
+    printf("\n enter the 2 number a to b");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+
+        if(i%2!=0)
+        {
+
+            printf("\t%d",i);
+        }
+    }
+}
+
