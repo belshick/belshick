0711fd5b04a3625a0af6af360943063143d3cded
belshick <belshick.megasoncse@ksrce.ac.in>
2017-01-25 12:19:49 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,12 @@
+package b;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		
+			   StringBuffer buffer = new StringBuffer("bell");
+			   buffer.reverse();
+			   System.out.println(buffer);
+			 }  
+			}
+
