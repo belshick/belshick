71d68f2a55a8e79ad6165830b1a5299ad27f8156
belshick <belshick.megasoncse@ksrce.ac.in>
2017-03-16 15:28:39 +0530

    Add files via upload

---
+++ FACT.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,i,f=1;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+
+        f=f*i;
+    }
+    printf("THE FACTORIAL NUMBER %d",f);
+    return 0;
+}
