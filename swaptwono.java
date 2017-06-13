6a75b01b6356fc8e7bb5ef815a259a8dc6744773
belshick <belshick.megasoncse@ksrce.ac.in>
2017-06-13 11:16:05 +0530

    Add files via upload

---
+++ swaptwono.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class swaptwono {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.println("After Swap:"+a+" "+b);
+		int t;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println("Before Swap:"+a+" "+b);
+
+	}
+
+}
