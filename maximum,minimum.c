83bfea617866e25299cad381b9589500d887ae6e
belshick <belshick.megasoncse@ksrce.ac.in>
2017-06-28 02:22:55 -0700

    Add files via upload

---
+++ maximum,minimum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int arr[100];
+  int n,i;
+  int max=arr[0];
+  int min=arr[0];
+  printf("enter the number");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&arr[i]);
+  }
+   for(i=0;i<n;i++)
+   {
+   if(arr[i]>max)
+   {
+   arr[i]=max;
+   }
+   }
+   printf("%d",max);
+
+}
