d537458e7a2f2fedfc65af4e060a57017d583217
belshick <belshick.megasoncse@ksrce.ac.in>
2017-07-23 21:05:55 -0700

    Add files via upload

---
+++ Set5Pro45.java
@@ -0,0 +1,20 @@
+package Hunder;
+
+public class Set5Pro45 {
+
+	
+	public static void main(String[] args) {
+		String[] string={"AL","AL","GH","F","GH","PK"};
+		//int len=string.length;
+		for(int i=0;i<string.length;i++)
+		{
+			if(string[i].length()==1)
+			{
+				System.out.println(string[i]);
+			}
+		}
+		
+
+	}
+
+}
