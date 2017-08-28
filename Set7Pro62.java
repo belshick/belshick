d3dbb70c090bb9c38a88eb00cd7ae4393b18fdd3
belshick <belshick.megasoncse@ksrce.ac.in>
2017-08-28 05:17:35 -0700

    Add files via upload

---
+++ Set7Pro62.java
@@ -0,0 +1,42 @@
+package Pro;
+import java.util.*;
+public class Set7Pro62 {
+
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a word");
+		String str=sc.next();
+		String sub;
+		int a=0;
+		String s = null;
+		int len=str.length();
+		for(int i=0;i<len;i++)
+		{
+			for(int j=i+1;j<len;j++)
+			{
+				if(str.charAt(i)==str.charAt(j))
+				{
+					sub=str.substring(i,j+1);
+					StringBuffer sb=new StringBuffer(sub);
+					StringBuffer s2=sb.reverse();
+					String ss=new String(s2);
+					if(sub.equals(ss))
+					{
+						int l1=sub.length();
+						if(l1>a)
+						{
+							s=sub;
+							a=l1;
+						}
+					}
+					
+					
+				}
+			}
+		}
+		System.out.println(s);
+
+	}
+
+}
