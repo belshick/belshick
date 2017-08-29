275dcd0dcbe9cba0b067346cb7f5154c17b34647
belshick <belshick.megasoncse@ksrce.ac.in>
2017-08-29 06:13:10 -0700

    Add files via upload

---
+++ Set6Pro51.java
@@ -0,0 +1,15 @@
+package Hunder;
+import java.util.*;
+public class Set6Pro51 {
+
+	
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		String n1 = a.next();
+		String n2 = a.next();
+		String ans = Integer.toString(Integer.parseInt(n1) * Integer.parseInt(n2));
+		System.out.println("Multiplecation of the numbers as a string:"+" "+ans);
+
+	}
+
+}
